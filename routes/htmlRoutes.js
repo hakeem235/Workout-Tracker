758fed1316555065e98aa23d6d7243c1920afd62
ahmed hakeem <hakeemkoo@hotmail.com>
2021-06-16 12:23:31 -0400

    add htmlRoutes.js on the routesflie

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+const path = require("path");
+
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
+});
+
+router.get("/exercise", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
+});
+
+router.get("/stats", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
+});
+
+module.exports = router;
