4e4c49b744474319633b19b8a9b8b3a89fba5154
ahmed hakeem <hakeemkoo@hotmail.com>
2021-06-17 10:53:12 -0400

    fix borken file

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.static("public"));
 
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/lets-workout", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutDB", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
